2461ad9c76e9527dc1607f9736ac35197d4637fa
Myeongjin Kim <myeongjin1115@gmail.com>
2020-01-02 15:20:02 +0900

    Updates

---
+++ precache-manifest.22f4e462c9514470e33dfb9d05797aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2900d9aba2c377fbf4082ea1d546bc4e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cd9b74f88b3e9975a304",
+    "url": "/my-app/static/css/main.f0e0c986.chunk.css"
+  },
+  {
+    "revision": "bb974403da00fa96886c",
+    "url": "/my-app/static/js/2.3196dca3.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/my-app/static/js/2.3196dca3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd9b74f88b3e9975a304",
+    "url": "/my-app/static/js/main.075f7c33.chunk.js"
+  },
+  {
+    "revision": "1d0d1868f76663eec549",
+    "url": "/my-app/static/js/runtime-main.467216da.js"
+  }
+]);
